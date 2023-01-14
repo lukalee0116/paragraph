956fd5fd3abf6a8f136918ecb2f1d9a1318af227
Luka Lee <lukalee0116@gmail.com>
2023-01-14 12:24:42 -0800

    First commit

---
+++ src/Main.java
@@ -0,0 +1,164 @@
+public class Main {
+    public static void main(String[] args) {
+        String myString = "I love coins, I want a silver eagle and a gold eagle";
+        if (myString== "");
+            System.out.println(myString);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  //   {
+     //   displayHighScorePosition("Luka", calculateHighScorePosition(100));
+     //   displayHighScorePosition("Arsene", calculateHighScorePosition(80));
+    //    displayHighScorePosition("Linda",  calculateHighScorePosition(70));
+   //     displayHighScorePosition("cara", calculateHighScorePosition(50));
+   //     return;
+
+  //  }
+
+  //  public static void nba() {
+    //    System.out.println("I love nba");
+  //  }
+
+   // public static void displayHighScorePosition(String name, int highScore) {
+   //     System.out.println("this person's " + name + " is ");
+    //    System.out.println("score: " + highScore);
+   // }
+
+   // public static int calculateHighScorePosition(int playerScore) {
+   //     if (playerScore >= 100) {
+    //        return 1;
+   //     }
+   //     if(playerScore >= 100) {
+    //        return 1;
+      //  } else if(playerScore >= 80 && playerScore < 90) {
+     //       return 2;
+     //   } else if(playerScore >=70  && playerScore < 80) {
+     //       return 3;
+    //    } else {
+    //        return 4;
+   //     }
+   // }
+
+
+  //  {
+
+                   //       String arg1 = args[0];
+                   //       String arg2 = args[1];
+//        int myCoinValue = Integer.valueOf(arg1);
+                   //       int myCoinValue2 = Integer.valueOf(arg2);
+//
+                   //      int myCoinValue = 61;
+                   //      if (myCoinValue > 60) {
+                   //           System.out.println("if My coin is greater 60..." + myCoinValue);
+                   //      }
+//
+                   //          System.out.println("if My coin is greater 50..." + myCoinValue);
+                   //      } else {
+                   //          System.out.println("others: " + myCoinValue);
+                   //     }
+
+                   //      int i = myCoinValue > 60 ? 1 : 2;
+                   //     System.out.println("My coin is worth..." + myCoinValue);
+//
+    }
+}
